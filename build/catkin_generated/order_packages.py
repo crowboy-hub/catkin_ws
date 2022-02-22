b25442e1e8549a417dd230a13948b5fcf401b667
jackdaw <2069532522@qq.com>
2022-02-22 16:52:51 +0800

    first

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/jackdaw/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/jackdaw/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/jackdaw/catkin_ws/devel;/opt/ros/melodic' != '' else []
